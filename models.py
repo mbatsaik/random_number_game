57ebe6739e2229a4618c01de8b6c4a53eedf6ef1
Marco Gutierrez <a20141676@pucp.pe>
2020-12-17 23:36:07 -0500

    added stage 1 code changes

--- models.py
+++ models.py
@@ -1,10 +1,7 @@
 from otree.api import (
-    models, BaseConstants, BaseSubsession, BasePlayer, widgets,
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
 )
-
-from django.contrib.contenttypes.models import ContentType
-from otree_redwood.models import Event, DecisionGroup
-from otree_redwood.models import Group as RedwoodGroup
 
 import csv
 import random
@@ -18,64 +15,207 @@ doc = """
 This is a Lines Queueing project
 """
 
+# def parse_config(config_file):
+#     """
+#     Parses the information of a configuration file for otree usage
+
+#     Input: configuration file name (config_file)
+#     Output: None 
+#     """
+
+#     with open('random_number_game/configs/' + config_file) as file:
+#         rows = list(csv.DictReader(file))
+
+#     rounds = []
+#     for row in rows:
+#         rounds.append({
+#             'round_number': int(row['round_number']),
+#             'stage': int(row['stage']),
+#             'duration': int(row['duration']),
+#             'shuffle_role': True if row['shuffle_role'] == 'TRUE' else False,
+#             'players_per_group': int(row['players_per_group']),
+#         })
+#     return rounds
+
+
 class Constants(BaseConstants):
     name_in_url = 'random_number_game'
-    players_per_group = None
-    num_rounds = 50
+    players_per_group = 4
+    num_rounds = 150 # 50 rounds per stage to make the respective page repeat itself 50 times 
     base_points = 0
 
-
-def parse_config(config_file):
-    with open('random_number_game/configs/' + config_file) as f:
-        rows = list(csv.DictReader(f))
-
-    rounds = []
-    for row in rows:
-        rounds.append({
-            'round_number': int(row['round_number']),
-            'stage': int(row['stage']),
-            'duration': int(row['duration']),
-            'shuffle_role': True if row['shuffle_role'] == 'TRUE' else False,
-            'players_per_group': int(row['players_per_group']),
-        })
-    return rounds
 
 class Subsession(BaseSubsession):
 
     def num_rounds(self):
+        """
+        Defines the number of rounds of a session. In this case, 
+        it uses a config file for this task
+        """
         return len(parse_config(self.session.config['config_file']))
 
     def creating_session(self):
         config = self.config
-        if not config:
-            return
 
-        num_silos = self.session.config['num_silos']
-        fixed_id_in_group = not config['shuffle_role']
+        num_silos = self.session.config['num_silos'] #TODO: ask Alex what is num_silos: possible subgroup
+        fixed_id_in_group = not config['shuffle_role'] # if role not shuffled, the ids'll be fixed
 
         players = self.get_players()
         num_players = len(players)
         silos = [[] for _ in range(num_silos)]
+
+        # silo assignation per player in first round
         for i, player in enumerate(players):
-            if self.round_number == 1:
+            if self.round_number == 1: 
                 player.silo_num = math.floor(num_silos * i/num_players)
-            else:
+            else: # same silo for the next rounds
                 player.silo_num = player.in_round(1).silo_num
             silos[player.silo_num].append(player)
+        
+        # defining a matrix with players per group
         group_matrix = []
         for silo in silos:
             silo_matrix = []
-            ppg = num_players#self.config['players_per_group']
-            for i in range(0, len(silo), ppg):
-                silo_matrix.append(silo[i:i+ppg])
+            for i in range(0, len(silo), num_players):
+                silo_matrix.append(silo[i:i+num_players])
             group_matrix.extend(otree.common._group_randomly(silo_matrix, fixed_id_in_group))
-        self.set_group_matrix(group_matrix)
+        self.set_group_matrix(group_matrix) # setting up the matrix in otree
+    
+    def set_payoffs_per_group(self):
+        """
+        Sets the payoff for the participants in each group
+
+        Input: None
+        Output: None
+        """
+        for group in self.get_groups():
+            group.set_payoffs()
+
+
+class Group(BaseGroup):
+   
+    stage = models.IntegerField()
     
     def set_payoffs(self):
-        for g in self.get_groups():
-            g.set_payoffs()
+        #TODO: fix payoff function so that it uses set_payoff of player
+        events = list(self.events.filter(channel='number'))
+        for p in self.get_players():
+            p.set_correct_answers(events)
+        if self.stage() == 2:
+            for g in self.session.vars['gender_groups']:
+                print(g)
+                g.sort(key=lambda x: self.get_player_by_id(x.id_in_group).correct_answers(), reverse=True)
+                for p in g:
+                    print(p)
+                    print(p.id_in_group, " correct answers: ", self.get_player_by_id(p.id_in_group).correct_answers())
+        for p in self.get_players():
+            p.set_payoff()
 
+    # def _on_number_event(self, event=None, **kwargs):
+    #     print(event.value)
+    #     id = event.value['id']
+    #     player = self.get_player_by_id(int(id))
+
+    #     num_string = ""
+    #     for i in range(9):
+    #         num_string += str(random.randint(1, 9))
+    #     event.value['number'] = int(num_string)
+    #     event.value['channel'] = 'outgoing'
+
+    #     # broadcast the updated data out to all subjects
+    #     self.send('number', event.value)
+    #     self.save()
+
+
+
+class Player(BasePlayer):
+    silo_num = models.IntegerField()
+    task_number = models.StringField()
+    task_number_img = models.StringField() # command for displaying task_number image file
+    transcription = models.StringField()
+    _initial_number = models.IntegerField()
+    _correct_answers = models.IntegerField(initial=0)
+    _gender_group_id = models.IntegerField()
+
+    _gender =  models.StringField(
+        choices=[
+            'Male',
+            'Female',
+        ],
+        widget=widgets.RadioSelect,
+        label='Question: What is your gender?'
+    )
+    _choice =  models.IntegerField(
+        choices=[
+            1,
+            2
+        ],
+        widget=widgets.RadioSelect,
+        label='Question: What payment treatment?'
+    )
+
+    def set_correct_answer(self, transcription):
+        """
+        Verifies that the number inputted is correct and adds 
+        increases the count of correct answers if that's the
+        case
+
+        Input: transcripted number (integer)
+        Output: None
+        """
+        if transcription == self.task_number:
+            self._correct_answers += 1
+
+    def task_number_method(self):
+        """
+        Creates a random 9-digit number as a string for transcription 
+        with unique digits
+
+        Input: None
+        Output: task number (string)
+        """
+
+        random_number = ""
+        one_to_nine = [num for num in range(1, 10)] # list with numbers from 1 to 9
+
+        random.SystemRandom().shuffle(one_to_nine) # shuffling the order of its items
+
+        # turning the list into a string
+        for num in one_to_nine:
+            initial_number += str(num)
+
+        return random_number
+
+    def set_payoff(self):
+        print("set_payoff: ", self._correct_answers)
+        if self.group.stage == 1:
+            self.payoff = self._correct_answers * 1500
+        elif self.group.stage == 2:
+            self.payoff = self._correct_answers * 1500
+            # TODO: edit stage 3 payoff
+        elif self.group.stage == 2:
+            group = self.session.vars['gender_groups'][self.session.vars['gender_groups_ids'][self.id_in_group]]
+            if group[0].id_in_group == self.id_in_group:
+                self.payoff = self._correct_answers * 6000
+            else:
+                self.payoff = 0
+        elif self.group.stage == 3 and self._choice == 1:
+            self.payoff = self._correct_answers * 1500
+        elif self.group.stage == 3 and self._choice == 2:
+            group = self.session.vars['gender_groups'][self.session.vars['gender_groups_ids'][self.id_in_group]]
+            
+            newGroup = [(p.id_in_group , p.in_round(3)._correct_answers) for p in group if p.id_in_group is not self.id_in_group]
+            newGroup.append((self.id_in_group , self._correct_answers))
+            newGroup.sort(key=lambda x: x[1], reverse=True)
+
+            if newGroup[0][0] == self.id_in_group:
+                self.payoff = self._correct_answers * 6000
+            else:
+                self.payoff = 0
+
+"""
     def set_initial_numbers(self):
+        
         if self.round_number == 1:
             self.session.vars['gender_groups_ids'] = {}
             self.session.vars['gender_groups']  = []
@@ -127,107 +267,4 @@ class Subsession(BaseSubsession):
             return parse_config(self.session.config['config_file'])[self.round_number-1]
         except IndexError:
             return None
-
-class Group(RedwoodGroup):
-
-    def period_length(self):
-        return parse_config(self.session.config['config_file'])[self.round_number-1]['duration']
-    
-    def stage(self):
-        return parse_config(self.session.config['config_file'])[self.round_number-1]['stage']
-    
-    def set_payoffs(self):
-        events = list(self.events.filter(channel='number'))
-        for p in self.get_players():
-            p.set_correct_answers(events)
-        if self.stage() == 2:
-            for g in self.session.vars['gender_groups']:
-                print(g)
-                g.sort(key=lambda x: self.get_player_by_id(x.id_in_group).correct_answers(), reverse=True)
-                for p in g:
-                    print(p)
-                    print(p.id_in_group, " correct answers: ", self.get_player_by_id(p.id_in_group).correct_answers())
-        for p in self.get_players():
-            p.set_payoff()
-
-    def _on_number_event(self, event=None, **kwargs):
-        print(event.value)
-        id = event.value['id']
-        player = self.get_player_by_id(int(id))
-
-        num_string = ""
-        for i in range(9):
-            num_string += str(random.randint(1, 9))
-        event.value['number'] = int(num_string)
-        event.value['channel'] = 'outgoing'
-
-        # broadcast the updated data out to all subjects
-        self.send('number', event.value)
-        self.save()
-
-
-
-class Player(BasePlayer):
-    silo_num = models.IntegerField()
-    _initial_number = models.IntegerField()
-    _correct_answers = models.IntegerField(initial=0)
-    _gender_group_id = models.IntegerField()
-
-    _gender =  models.StringField(
-        choices=[
-            'Male',
-            'Female',
-        ],
-        widget=widgets.RadioSelect,
-        label='Question: What is your gender?'
-    )
-    _choice =  models.IntegerField(
-        choices=[
-            1,
-            2
-        ],
-        widget=widgets.RadioSelect,
-        label='Question: What payment treatment?'
-    )
-
-    def num_players(self):
-        return parse_config(self.session.config['config_file'])[self.round_number-1]['players_per_group']
-    
-    def initial_number(self):
-        return self._initial_number
-    
-    def correct_answers(self):
-        return self._correct_answers
-
-    def set_correct_answers(self, events):
-        correct_answers = 0
-        for event in events:
-            if event.value['id'] == self.id_in_group and event.value['channel'] == 'incoming':
-                correct_answers += 1
-        self._correct_answers = correct_answers
-
-    def set_payoff(self):
-        print("set_payoff: ", self._correct_answers)
-        if self.group.stage() == 0:
-            self.payoff = self._correct_answers * 1500
-        elif self.group.stage() == 1:
-            self.payoff = self._correct_answers * 1500
-        elif self.group.stage() == 2:
-            group = self.session.vars['gender_groups'][self.session.vars['gender_groups_ids'][self.id_in_group]]
-            if group[0].id_in_group == self.id_in_group:
-                self.payoff = self._correct_answers * 6000
-            else:
-                self.payoff = 0
-        elif self.group.stage() == 3 and self._choice == 1:
-            self.payoff = self._correct_answers * 1500
-        elif self.group.stage() == 3 and self._choice == 2:
-            group = self.session.vars['gender_groups'][self.session.vars['gender_groups_ids'][self.id_in_group]]
-            
-            newGroup = [(p.id_in_group , p.in_round(3)._correct_answers) for p in group if p.id_in_group is not self.id_in_group]
-            newGroup.append((self.id_in_group , self._correct_answers))
-            newGroup.sort(key=lambda x: x[1], reverse=True)
-
-            if newGroup[0][0] == self.id_in_group:
-                self.payoff = self._correct_answers * 6000
-            else:
-                self.payoff = 0
+"""
--- pages.py
+++ pages.py
@@ -1,15 +1,54 @@
 import math
+from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
-
+from .models import Constants
 from datetime import timedelta
 from operator import concat
 from functools import reduce
-from .models import parse_config
+
+from PIL import Image, ImageDraw, ImageFont
+from time import time
+
+
+def writeText(text, fileName):
+    """"
+    This method generates the image version of an inputted text
+    and saves it to fileName
+    
+    Input: text to be transcripted into the image, name of the output img file
+    Output: None
+    """
+
+    image = Image.open('random_number_game/background.png')
+    image = image.resize((650, 100))
+    image.save('random_number_game/background.png')
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('random_number_game/Roboto-Regular.ttf', size=19)
+    imageChars = 100
+    numLines = len(text) / imageChars
+    numLines = math.ceil(numLines)
+    lines = []
+
+    for i in range(numLines):
+        if(imageChars * (i + 1) < len(text)):
+            lines.append(text[imageChars * i : imageChars * (i+1)])
+        else:
+            lines.append(text[imageChars * i : len(text)])
+
+    for i in range(numLines):
+        (x, y) = (10, 20 * i)
+        message = lines[i]
+        print("Message is: ", message)
+        color = 'rgb(0, 0, 0)' # black color
+        draw.text((x, y), message, fill=color, font=font)
+
+    image.save(fileName) # stores the image on a specified folder
+
 
 class Introduction(Page):
-
     def is_displayed(self):
         return self.round_number == 1
+
 
 class GenderPage(Page):
     form_model = 'player'
@@ -18,41 +57,81 @@ class GenderPage(Page):
     def is_displayed(self):
         return self.round_number == 1
 
+    def before_next_page(self):
+        #TODO: look for another page that appears multiple times to create the images
+        # creating the number images and storing their commands to display on page
+        for player in self.group.get_players():
+            player.task_number = self.player.task_number_method()
+            self.round_number
+            # name of random number image file
+            task_number_file = "random_number_game/" + \
+                               f"{player.task_number}_player_{player.id_in_group}_{self.round_number}"
+            # creating the img file
+            writeText(player.task_number, f'random_number_game/static/{task_number_file}.png')
+
+            # storing the command to display the image per player
+            player.task_number_img = '<img src="{% static "' + f'{task_number_file}.png' + '" %}"/>'
+
+        # timeout for multiple pages that restarts at the beginning of first stage
+        #TODO: look for another page to reset the expiry time per beginning of stage
+        self.participant.vars['expiry_time'] = time() + 3*60 # timeout of 3 minutes
+
+
 class ChoicePage(Page):   
     form_model = 'player'
     form_fields = ['_choice']
 
     def is_displayed(self):
-        return self.group.stage() == 3
+        return self.round_number == round(2*Constants.num_rounds/3) + 1 # displays on beginning of third stage
 
-class DecisionWaitPage(WaitPage):
 
+class Stage2WaitPage(WaitPage):
+    """
+    WaitPage for assigning each player to their respective
+    2 men 2 women group for Stage 2
+    """
+    
     body_text = 'Waiting for all players to be ready'
     wait_for_all_groups = True
-    after_all_players_arrive = 'set_initial_numbers'
+    after_all_players_arrive = 'group_assignment' # players will be assigned into groups of 2 men and 2 women
+    #TODO: the group_assignment method requires some commands to keep the grouping as this round for the rest of rounds
 
     def is_displayed(self):
-        return self.subsession.config is not None
+        return self.round_number == round(Constants.num_rounds/3) + 1 # displays on beginning of second stage
 
+
+class ProcessingPage(Page):
+    timeout_seconds = 0.5 #TODO: test if non-integer timeouts work
+
+    #TODO: complete page for processing the image for the transcription task
 
 class Decision(Page):
+    form_model = "player"
+    form_fields = ['transcription']
+    
+    #TODO: code frontend page for decision
+    def get_timeout_seconds(self):
+        return self.participant.vars['expiry_time'] - time() # updating the time each time the page is displayed
 
     def is_displayed(self):
-        return self.subsession.config is not None
+        return self.participant.vars['expiry_time'] != 0 # display only if there is time left
+
+    def before_next_page(self):
+        self.player.set_correct_answer(self.player.transcription) # checking if the answer was correct
 
 
 class ResultsWaitPage(WaitPage):
     wait_for_all_groups = True
-
     after_all_players_arrive = 'set_payoffs'
 
     def is_displayed(self):
-        return self.subsession.config is not None
+        return self.round_number == round(Constants.num_rounds/3) or \
+               self.round_number == round(2*Constants.num_rounds/3)
+
+    #TODO: add after all players arrive method for setting every player's payoff
 
 
 class Results(Page):
-
-    #timeout_seconds = 15
 
     def is_displayed(self):
         return self.subsession.config is not None
@@ -82,8 +161,9 @@ class Payment(Page):
 page_sequence = [
     Introduction,
     GenderPage,
-    DecisionWaitPage,
     ChoicePage,
+    Stage2WaitPage,
+    ProcessingPage,
     Decision,
     ResultsWaitPage,
     Results,
